06253e2e68835bd410a6b38c08ccdfd276a6b505
AbubakerElsiddig <abobkr270@gmail.com>
2025-05-03 12:32:40 +0300

    تدريب نموذج الذكائ الاصطناعي و تجزئة المراجعات

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # بسم الله الرحمن الرحيم
+
 import pandas as pd
 from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
 import re
-# import numpy as np
-# import nltk
+from keras_preprocessing.text import Tokenizer
+# import os
+# os.environ["KERAS_BACKEND"] = "jax"
 
-#nltk.download('stopwords')
+
+# nltk.download('stopwords')
 # print(stopwords.words('english'))
+
 
 df = pd.read_csv('chatgpt_reviews.csv')
 df = df.dropna()
@@ -28,4 +32,20 @@ df['label'] = df['label'].map({'POSITIVE': 1, 'NEGATIVE': 0})
 df.to_csv('clean_reviews.csv', index=False)
 print("Cleaned data saved to 'clean_reviews.csv'")
 
+# Split data
+X = df['Review']
+y = df['label']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
 
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+X_train_seq = tokenizer.texts_to_sequences(X_train)
+X_test_seq = tokenizer.texts_to_sequences(X_test)
+
+max_length = max(len(seq) for seq in X_train_seq)
+vocab_size = len(tokenizer.word_index) + 1
+
+print(vocab_size)
